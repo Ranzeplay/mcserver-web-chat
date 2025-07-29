ea68f94577b9900e1e5cbec6c4ebf7622f2c38bd
Jeb Feng <ranzeplay@outlook.com>
2025-07-29 14:15:40 +0800

    Initial commit

---
+++ common/src/main/java/space/ranzeplay/serverwebchat/Constants.java
@@ -0,0 +1,11 @@
+package space.ranzeplay.serverwebchat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "serverwebchat";
+    public static final String MOD_NAME = "ServerWebChat";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/space/ranzeplay/serverwebchat/platform/FabricPlatformHelper.java
@@ -0,0 +1,22 @@
+package space.ranzeplay.serverwebchat.platform;
+
+import space.ranzeplay.serverwebchat.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
